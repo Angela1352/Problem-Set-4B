c755d27a2d5e8deb541ebc600e785206937e08b0
Angela Chen <84543226+Angela1352@users.noreply.github.com>
2023-10-23 11:30:48 -0700

    Add files via upload

--- Problem Set 4B/Problem_Set_4B.java
+++ Problem Set 4B/Problem_Set_4B.java
@@ -9,35 +9,67 @@ import java.util.Scanner;
 
 public class Problem_Set_4B {
     public static void main (String[] args) {
-        
+
         Scanner input = new Scanner(System.in);
-        
+
         String nonLetters = ",.:;()[]{}!?@#$%^&* ";
         boolean palindrome = true;
-        
+
         System.out.print("Palindrome: ");
         String pal = input.nextLine();
         System.out.println();
-    
+
         pal = pal.toLowerCase();    
-        
+
         int first = 0;
         int last = pal.length()-1;
         String letter = pal.substring(first, first+1);
-        
+
         while (first < last) {
             char Lchar = pal.charAt(first);
             char Rchar = pal.charAt(last);
-            
+
             if (nonLetters.contains(letter)) first++;
-            if (Lchar != Rchar) palindrome = false;
-            
+            else if (Lchar != Rchar) palindrome = false;
+
             first++;
             last--;
         }
         
         System.out.println(palindrome);
-        
+
+        /*
+        // -----------------------------------------------------------------------
+
+        String result = "";
+
+        System.out.println();
+        System.out.println("Pig Latin: ");
+        String pig = input.nextLine();
+        System.out.println();
+
+        for (int i = 0; i < pig.length(); i++) {
+
+        String word = pal.substring(0, i);
+        }
+
+        //if (word.length() <= 2) System.out.println(pal);
+
+        for (int i = 0; i < pig.length(); i++) {
+        result += pig.substring(1) + pig.substring(i, i+1) + "ay";
+        }
+        System.out.println(result);
+
+        // -----------------------------------------------------------------------
+         */
+
     }
 }
+
+
+
+
+
+
+
 
